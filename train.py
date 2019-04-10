d9fda59b64df5f645290a6c6fca065223f0ebce9
ace19-dev <acemc19@gmail.com>
2019-04-10 10:37:08 +0900

    update

--- train.py
+++ train.py
@@ -39,7 +39,7 @@ flags.DEFINE_string('summaries_dir', './models/train_logs',
 
 flags.DEFINE_enum('learning_policy', 'poly', ['poly', 'step'],
                   'Learning rate policy for training.')
-flags.DEFINE_float('base_learning_rate', .0001,
+flags.DEFINE_float('base_learning_rate', .00015,
                    'The base learning rate for model training.')
 flags.DEFINE_float('learning_rate_decay_factor', 1e-3,
                    'The rate to decay the base learning rate.')
